6f9472d742877bfa5280654a06118b567aa05425
Keiichiro Sonoda <keison8864@gmail.com>
2024-06-15 10:43:53 +0900

    標準偏差を変えてグラフを確認

--- step01/norm_param.py
+++ step01/norm_param.py
@@ -18,16 +18,39 @@ def normal(x: float, mu: float = 0, sigma: float = 1) -> float:
 
 x = np.linspace(-10, 10, 1000)
 
+# 平均値を動かす
 y0 = normal(x, mu=-3)
 y1 = normal(x, mu=0)
 y2 = normal(x, mu=5)
 
-fig = plt.figure(figsize=(8, 6))
-ax = fig.add_subplot(111)
+# 標準偏差を動かす
+y3 = normal(x, sigma=0.5)
+y4 = normal(x, sigma=1)
+y5 = normal(x, sigma=2)
 
-ax.plot(x, y0, label=r'$\mu=-3$')
-ax.plot(x, y1, label=r'$\mu=0$')
-ax.plot(x, y2, label=r'$\mu=5$')
-ax.legend()
-ax.grid()
+fig = plt.figure(figsize=(8, 8))
+
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+
+ax1.plot(x, y0, label=r'$\mu=-3$')
+ax1.plot(x, y1, label=r'$\mu=0$')
+ax1.plot(x, y2, label=r'$\mu=5$')
+
+ax2.plot(x, y3, label=r'$\sigma=0.5$')
+ax2.plot(x, y4, label=r'$\sigma=1$')
+ax2.plot(x, y5, label=r'$\sigma=2$')
+
+ax1.legend()
+ax1.grid()
+ax1.set_xlabel('x')
+ax1.set_ylabel('y')
+ax1.set_title('Mean')
+
+ax2.legend()
+ax2.grid()
+ax2.set_xlabel('x')
+ax2.set_ylabel('y')
+ax2.set_title('Standard deviation')
+
 plt.show()
