aeea5d2f03891ef77a7054e17ba5c156fd9a73f6
Keiichiro Sonoda <keison8864@gmail.com>
2024-07-06 10:08:51 +0900

    身長のヒストグラムと推定した正規分布をプロット。

--- step02/hist.py
+++ step02/hist.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
+def normal(x: float, mu: float = 0, sigma: float = 1) -> float:
+    y = 1 / (np.sqrt(2 * np.pi) * sigma) * np.exp(-(x - mu) ** 2 / (2 * sigma ** 2))
+    return y
+
 path = os.path.dirname(__file__) / Path('height.txt')
 
 # SOCR Data を使用。
@@ -11,8 +15,19 @@ path = os.path.dirname(__file__) / Path('height.txt')
 xs = np.loadtxt(path)
 print(xs.shape) # (25000,)
 
+# サンプルの平均と標準偏差。
+mu = np.mean(xs)
+sigma = np.std(xs)
+
+print(mu)
+print(sigma)
+
+x = np.linspace(150, 190, 1000)
+y = normal(x, mu, sigma)
+
 fig, ax = plt.subplots(1, 1, figsize=(8, 6))
 ax.hist(xs, bins='auto', density=True)
+ax.plot(x, y)
 ax.set_xlabel('Height(cm)')
 ax.set_ylabel('Probability Density')
 ax.grid()
